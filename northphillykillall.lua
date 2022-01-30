77f6f386e899b67fc6abf2f82b1073bc12c912c5
stillnotgivingafuck <90551400+stillnotgivingafuck@users.noreply.github.com>
2022-01-30 12:57:24 +0000

    Update northphillykillall.lua

--- northphillykillall.lua
+++ northphillykillall.lua
@@ -5,8 +5,8 @@ https://www.roblox.com/games/8507378236/North-Philly-Being-made# ]]--
 local _______ = game:GetService("Players").LocalPlayer.Character.Glock.DamageEvent
 local ______ = game.Players.LocalPlayer
 local _____ = game.Players:GetPlayers()
-local ____ = Character
-local _ = Humanoid
+local ____ = "Character"
+local _ = "Humanoid"
 for __ = 1,15 do
     for __, ___ in next, _____ do
         if ___ ~= ______ then
