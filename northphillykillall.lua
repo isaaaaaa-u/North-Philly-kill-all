aa97bb206e2b03a656cc331c9290d1ebe1bf59c0
stillnotgivingafuck <90551400+stillnotgivingafuck@users.noreply.github.com>
2022-01-30 14:03:34 +0000

    Update northphillykillall.lua

--- northphillykillall.lua
+++ northphillykillall.lua
@@ -2,17 +2,26 @@
 Hold out glock then execute
 https://www.roblox.com/games/8507378236/North-Philly-Being-made# ]]--
 
-local _______ = game:GetService("Players").LocalPlayer.Character.Glock.DamageEvent
-local ______ = game.Players.LocalPlayer
-local _____ = game.Players:GetPlayers()
+local _______________ = "GetPlayers"
+local ______________ = "DamageEvent"
+local _____________ = "Glock"
+local ____________ = "Character"
+local ___________ = ____________._____________.______________
+local __________ = "LocalPlayer"
+local _________ = "FireServer"
+local ________ = game.Players
+local _______ = ______.___________
+local ______ = ________.__________
+local _____ = ________:_______________()
 local ____ = "Character"
 local _ = "Humanoid"
 for __ = 1,15 do
     for __, ___ in next, _____ do
         if ___ ~= ______ then
-            _______:FireServer(___.____._)
+            _______:_________(___.____._)
         end
     end
 end
 
----yess we do trollage
+--[[yess we do trollage
+cancerous code goes uwu]]--
